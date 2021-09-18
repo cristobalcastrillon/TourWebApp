fe96b1632825e1611f5442ba73b69a10106e9ba2
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-09-18 14:28:49 -0500

    Project set (dependencies and whatnot).

---
+++ src/main/java/com/webdev/tourapp/TourappApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.tourapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdev/tourapp/TourappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.tourapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
