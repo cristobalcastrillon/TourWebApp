ae410fe60e2bc7aa359ca929a0867e7dff066336
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-12 20:16:13 -0500

    Custom UUID implemented.

--- src/main/java/com/webdev/tourapp/Users/Guide/Domain/ValueObjects/GuideID.java
+++ src/main/java/com/webdev/tourapp/Users/Guide/Domain/ValueObjects/GuideID.java
@@ -1,6 +1,6 @@
 package com.webdev.tourapp.Users.Guide.Domain.ValueObjects;
 
-import com.webdev.tourapp.Shared.Domain.Aggregates.CustomUUID; 
+import com.webdev.tourapp.Shared.Domain.Aggregate.CustomUUID;
 public class GuideID extends CustomUUID
 {
 
--- src/main/java/com/webdev/tourapp/Users/User/Domain/ValueObjects/Username.java
+++ src/main/java/com/webdev/tourapp/Users/User/Domain/ValueObjects/Username.java
@@ -1,6 +1,6 @@
 package com.webdev.tourapp.Users.User.Domain.ValueObjects;
 
-import com.webdev.tourapp.Shared.Domain.Aggregates.StringObjectValue; 
+import com.webdev.tourapp.Shared.Domain.Aggregate.StringObjectValue;
 public class Username extends StringObjectValue
 {
 
