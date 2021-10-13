9a1e362b9969eac4b67ae4dd80dd283c53cee647
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-12 21:08:24 -0500

    TourName implemented.

---
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Domain/Exceptions/TourNameNotValid.java
@@ -0,0 +1,5 @@
+package com.webdev.tourapp.Tours.Tour.Domain.Exceptions;
+
+public class TourNameNotValid extends RuntimeException {
+    public TourNameNotValid(String message) {super(message);}
+}
