3d2efa3df5fdc2ea7dfc6ef44bb9fa14268a7ffb
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-12 20:55:35 -0500

    TourID implemented.

---
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Domain/ValueObjects/TourID.java
@@ -0,0 +1,11 @@
+package com.webdev.tourapp.Tours.Tour.Domain.ValueObjects;
+
+import com.webdev.tourapp.Shared.Domain.Aggregate.CustomUUID;
+
+public class TourID extends CustomUUID {
+
+    public TourID(String value) {
+        super(value);
+    }
+
+}
