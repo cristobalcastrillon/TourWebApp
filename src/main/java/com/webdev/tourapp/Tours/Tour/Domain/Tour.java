d6c5c0d5bc9bf31e692c889a3a1c64b4307e8ba0
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-13 21:05:23 -0500

    'Location' entity (in Tour bounded context) basic implementation done.

--- src/main/java/com/webdev/tourapp/Tours/Tour/Domain/Tour.java
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Domain/Tour.java
@@ -1,29 +1,36 @@
 package com.webdev.tourapp.Tours.Tour.Domain;
 
+import com.webdev.tourapp.Tours.Tour.Domain.Entities.Location;
 import com.webdev.tourapp.Tours.Tour.Domain.ValueObjects.TourID;
 import com.webdev.tourapp.Tours.Tour.Domain.ValueObjects.TourName;
 import com.webdev.tourapp.Tours.Tour.Domain.ValueObjects.TourPrice;
+
+import java.util.List;
 
 public class Tour {
     private TourID tourID;
     private TourName tourName;
     private TourPrice tourPrice;
+    private List<Location> locationsIncludedInTour;
 
     private Tour(){}
 
     public Tour(TourID id,
                 TourName name,
-                TourPrice price){
+                TourPrice price,
+                List<Location> locationsInTour){
         this.tourID = id;
         this.tourName = name;
         this.tourPrice = price;
+        this.locationsIncludedInTour = locationsInTour;
     }
 
     public static Tour Create(TourID id,
                               TourName name,
-                              TourPrice price){
+                              TourPrice price,
+                              List<Location> locationsInTour){
 
-        Tour tour = new Tour(id, name, price);
+        Tour tour = new Tour(id, name, price, locationsInTour);
         // EVENTS
         return tour;
     }
