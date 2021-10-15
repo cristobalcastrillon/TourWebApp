3dbe5ea8ace124757759c357e033d4d7c93ea7df
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-15 10:41:18 -0500

    Tour's findByID use case implemented.

--- src/main/java/com/webdev/tourapp/Tours/Tour/Application/FindByID/TourIDFinder.java
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Application/FindByID/TourIDFinder.java
@@ -1,4 +1,20 @@
 package com.webdev.tourapp.Tours.Tour.Application.FindByID;
 
+import com.webdev.tourapp.Tours.Tour.Domain.Ports.TourRepository;
+import com.webdev.tourapp.Tours.Tour.Domain.Services.DomainTourIDFinder;
+import com.webdev.tourapp.Tours.Tour.Domain.Tour;
+
 public class TourIDFinder {
+
+    private TourRepository repository;
+    private DomainTourIDFinder finderService;
+
+    public TourIDFinder(TourRepository repository){
+        this.repository = repository;
+        finderService = new DomainTourIDFinder(this.repository);
+    }
+
+    public Tour execute(String id){
+        return finderService.execute(id);
+    }
 }
--- src/main/java/com/webdev/tourapp/Tours/Tour/Domain/Ports/TourRepository.java
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Domain/Ports/TourRepository.java
@@ -2,6 +2,7 @@ package com.webdev.tourapp.Tours.Tour.Domain.Ports;
 
 import com.webdev.tourapp.Tours.Tour.Domain.Entities.Location;
 import com.webdev.tourapp.Tours.Tour.Domain.Tour;
+import com.webdev.tourapp.Tours.Tour.Domain.ValueObjects.TourID;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -11,7 +12,7 @@ public interface TourRepository {
     void update(Tour tour);
     void delete();
     Optional<ArrayList<Tour>> all();
-    Optional<Tour> findByID(String tourID);
+    Optional<Tour> findByID(TourID tourID);
     //TODO: Implement the Use Cases that use the following
     Optional<ArrayList<Tour>> findByTourName(String nameQuery);
     Optional<ArrayList<Tour>> findByListOfLocations(ArrayList<Location> locationList);
