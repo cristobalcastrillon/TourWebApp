569d395386d1b30e0067f8cf69f43d633c33b336
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-15 12:53:33 -0500

    TourBasePriceMaximumFinder implemented.

---
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Application/FindByBasePrice/TourBasePriceMaximumFinder.java
@@ -0,0 +1,25 @@
+package com.webdev.tourapp.Tours.Tour.Application.FindByBasePrice;
+
+import com.webdev.tourapp.Tours.Tour.Domain.Ports.TourRepository;
+import com.webdev.tourapp.Tours.Tour.Domain.Services.DomainTourBasePriceMaximumFinder;
+import com.webdev.tourapp.Tours.Tour.Domain.Tour;
+
+import java.util.ArrayList;
+
+/*
+* Este caso de uso busca los tours cuyo precio base es * menor o igual * al especificado (maxBasePrice)
+*/
+
+public class TourBasePriceMaximumFinder {
+    private TourRepository repository;
+    private DomainTourBasePriceMaximumFinder finderService;
+
+    public TourBasePriceMaximumFinder(TourRepository repository){
+        this.repository = repository;
+        finderService = new DomainTourBasePriceMaximumFinder(this.repository);
+    }
+
+    public ArrayList<Tour> execute(Double maxBasePrice){
+        return finderService.execute(maxBasePrice);
+    }
+}
