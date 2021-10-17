7f44c2e6dc165a0eb203ecef3d6a6e324f7ca694
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-16 21:41:40 -0500

    TourInstanceCreator basic implementation done. Not tested yet.

---
+++ src/main/java/com/webdev/tourapp/Tours/TourInstance/Application/Create/TourInstanceCreator.java
@@ -0,0 +1,51 @@
+package com.webdev.tourapp.Tours.TourInstance.Application.Create;
+
+import com.webdev.tourapp.Tours.Tour.Domain.Exceptions.TourAlreadyExists;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Entities.StartingLocation;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Entities.TourGuide;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Entities.TourUser;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Entities.TransportCompanyHired;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Ports.TourInstanceRepository;
+import com.webdev.tourapp.Tours.TourInstance.Domain.TourInstance;
+import com.webdev.tourapp.Tours.TourInstance.Domain.ValueObjects.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public class TourInstanceCreator{
+
+    private TourInstanceRepository repository;
+
+    public TourInstanceCreator(TourInstanceRepository repository){
+        this.repository = repository;
+    }
+
+    public void execute(Date tourDate,
+                        String id,
+                        Integer numberOfPersons,
+                        Double totalPrice,
+                        String status,
+                        TourGuide guide,
+                        StartingLocation startingLocation,
+                        Optional<List<TourUser>> usersInTour,
+                        Optional<TransportCompanyHired> companyHired){
+        this.validate(id);
+        TourInstance tourInstance = TourInstance.Create(new TourDate(tourDate),
+                new TourInstanceID(id),
+                new TourNumberOfPersons(numberOfPersons),
+                new TourTotalPrice(totalPrice),
+                new TourInstanceStatus(status),
+                guide,
+                startingLocation,
+                usersInTour,
+                companyHired);
+    }
+
+    private void validate(String tourID){
+        Optional<TourInstance> tour = repository.findByID(new TourInstanceID(tourID));
+        if(tour.isPresent()){
+            throw new TourAlreadyExists("El tour con id " + tourID.toString() + " ya existe.");
+        }
+    }
+}
--- src/main/java/com/webdev/tourapp/Tours/TourInstance/Domain/Ports/TourInstanceRepository.java
+++ src/main/java/com/webdev/tourapp/Tours/TourInstance/Domain/Ports/TourInstanceRepository.java
@@ -1,7 +1,7 @@
 package com.webdev.tourapp.Tours.TourInstance.Domain.Ports;
 
 import com.webdev.tourapp.Tours.TourInstance.Domain.TourInstance;
-import com.webdev.tourapp.Tours.TourInstance.Domain.ValueObjects.TourInstanceID;
+import com.webdev.tourapp.Tours.TourInstance.Domain.ValueObjects.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +13,8 @@ public interface TourInstanceRepository {
     Optional<List<TourInstance>> all();
     Optional<TourInstance> findByID(TourInstanceID id);
     //TODO: Implement the Use Cases that use the following
-    //TODO: Implement findByDate()
-    Optional<List<TourInstance>> findByDate();
-    Optional<List<TourInstance>> findByStatus(String status);
-    Optional<List<TourInstance>> findByNumberOfPersons(Integer numberOfPersons);
-    Optional<List<TourInstance>> findByTotalPrice(Double totalPrice);
+    Optional<List<TourInstance>> findByDate(TourDate date);
+    Optional<List<TourInstance>> findByStatus(TourInstanceStatus status);
+    Optional<List<TourInstance>> findByNumberOfPersons(TourNumberOfPersons numberOfPersons);
+    Optional<List<TourInstance>> findByTotalPrice(TourTotalPrice totalPrice);
 }
