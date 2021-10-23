9fbddbf7c4b13602ca98b4e2ed242f1c89ce3a04
RenixExtreme <ivanrene10@gmail.com>
2021-10-23 00:35:10 -0500

    Antes de mergue

--- src/main/java/com/webdev/tourapp/Cities/Domain/City.java
+++ src/main/java/com/webdev/tourapp/Cities/Domain/City.java
@@ -1,6 +1,43 @@
 package com.webdev.tourapp.Cities.Domain;
 
+import com.webdev.tourapp.Cities.Domain.ValueObjects.CityDefaultIATA;
+import com.webdev.tourapp.Cities.Domain.ValueObjects.CityID;
+import com.webdev.tourapp.Cities.Domain.ValueObjects.CityName;
+
+import java.util.Objects;
+
 public class City
 {
+    private CityID id;
+    private CityName name;
+    private CityDefaultIATA iata;
 
+
+    public City(CityID id, CityName name, CityDefaultIATA iata) {
+        this.id = id;
+        this.name = name;
+        this.iata = iata;
+
+
+    }
+    public static City Create(CityID id, CityName name, CityDefaultIATA iata){
+        City city = new City(id, name, iata);
+
+          // Pasos intermedios
+
+          return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(id, city.id) && Objects.equals(name, city.name) && Objects.equals(iata, city.iata);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, iata);
+    }
 }
--- src/main/java/com/webdev/tourapp/Cities/Domain/Ports/CityRepository.java
+++ src/main/java/com/webdev/tourapp/Cities/Domain/Ports/CityRepository.java
@@ -3,12 +3,16 @@ package com.webdev.tourapp.Cities.Domain.Ports;
 import com.webdev.tourapp.Cities.Domain.City;
 import com.webdev.tourapp.Cities.Domain.ValueObjects.CityID;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface CityRepository{
     void save(City city);
     Optional<City> find(CityID id);
+    /*
     void update(City city);
-    Optional<List<City>> all();
+    Optional<ArrayList<City>> all();
     void delete(City city);
     // Aqui falta algo pero no C
-
+    */
 }
