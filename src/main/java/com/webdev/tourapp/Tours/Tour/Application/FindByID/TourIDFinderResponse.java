3ecdc35e13200d7ba1af0abdb75c0c71d4d7f985
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-25 02:22:09 -0500

    FindTourInstanceByIDGetController implementation complete.

--- src/main/java/com/webdev/tourapp/Tours/Tour/Application/FindByID/TourIDFinderResponse.java
+++ src/main/java/com/webdev/tourapp/Tours/Tour/Application/FindByID/TourIDFinderResponse.java
@@ -5,15 +5,15 @@ import com.webdev.tourapp.Tours.Tour.Domain.Tour;
 import java.util.HashMap;
 
 public class TourIDFinderResponse {
-    private Tour tourResponse;
+    private Tour tour;
 
-    public TourIDFinderResponse(Tour tourResponse){
-        this.tourResponse = tourResponse;
+    public TourIDFinderResponse(Tour tour){
+        this.tour = tour;
     }
 
     public HashMap<String, Object> response() {
         HashMap<String, Object> response = new HashMap<>();
-        response.put("tour", tourResponse.data());
+        response.put("tour", tour.data());
         return response;
     }
 }
