2760d3bb036bb5f494edc6a41caa482635a46194
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-10-25 13:53:42 -0500

    ExceptionHandlers added for AllTourInstancesGetController & FindTourInstanceByIDGetController.

--- src/main/java/com/webdev/tourapp/Tours/TourInstance/Infrastructure/Controllers/AllTourInstancesGetController.java
+++ src/main/java/com/webdev/tourapp/Tours/TourInstance/Infrastructure/Controllers/AllTourInstancesGetController.java
@@ -2,12 +2,11 @@ package com.webdev.tourapp.Tours.TourInstance.Infrastructure.Controllers;
 
 import com.webdev.tourapp.Tours.TourInstance.Application.All.TourInstanceAll;
 import com.webdev.tourapp.Tours.TourInstance.Application.All.TourInstanceAllResponse;
+import com.webdev.tourapp.Tours.TourInstance.Domain.Exceptions.NoTourInstancesFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,5 +22,14 @@ public class AllTourInstancesGetController {
     public ResponseEntity<List<HashMap<String, Object>>> execute() {
         TourInstanceAllResponse response = all.execute();
         return ResponseEntity.status(HttpStatus.OK).body(response.response());
+    }
+
+    @ExceptionHandler(NoTourInstancesFound.class)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity handleNoTourInstancesFound(NoTourInstancesFound exception) {
+        HashMap<String, String> response = new HashMap<>() {{
+            put("error", exception.getMessage());
+        }};
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
     }
 }
