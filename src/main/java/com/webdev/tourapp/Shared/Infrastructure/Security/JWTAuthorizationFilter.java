8431c8f900a962749475b74c7e21ec6b4aac34bb
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2021-11-24 18:06:29 -0500

    Add files via upload

---
+++ src/main/java/com/webdev/tourapp/Shared/Infrastructure/Security/JWTAuthorizationFilter.java
@@ -0,0 +1,74 @@
+package com.webdev.tourapp.Shared.Infrastructure.Security;
+
+import com.webdev.tourapp.Shared.Domain.Exceptions.UnauthorizedAction;
+import io.jsonwebtoken.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter
+{
+
+    private final String HEADER = "token";
+    private final String PREFIX = "Bearer ";
+    private final String SECRET = "vewysecwet_key";
+    private final String CLAIM = "authorities";
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if (existJWTToken(request)) {
+                Claims claims = validateToken(request);
+                if (claims.get(CLAIM) != null) {
+                    setUpSpringAuthentication(claims);
+                } else {
+                    SecurityContextHolder.clearContext();
+                }
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        }
+        catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+            return;
+        }
+    }
+    //Valida que el JWT Token que tengan el HEADER y el PREFIJO.
+    private boolean existJWTToken(HttpServletRequest request) {
+        String authenticationHeader = request.getHeader(HEADER);
+        if(authenticationHeader == null || !authenticationHeader.startsWith(PREFIX)) {
+            return false;
+        }
+        return true;
+    }
+    //Valida si la firma (SECRET) Sea valida.
+    private Claims validateToken(HttpServletRequest request) {
+        try {
+
+            String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+            return Jwts.parser().setSigningKey(SECRET.getBytes()).parseClaimsJws(jwtToken).getBody();
+        }
+        catch (Exception e) {
+            throw new UnauthorizedAction("Servicio no Autorizado");
+        }
+    }
+
+    private void setUpSpringAuthentication(Claims claims) {
+        List<String> authorities = (List) claims.get(CLAIM);
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(),
+                null, authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+    }
+}
