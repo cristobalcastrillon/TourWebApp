a50a2dc215b26b2f910ec5766be1e1f63e198571
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2021-11-24 22:41:10 -0500

    Update WebSecurityConfig.java

--- src/main/java/com/webdev/tourapp/Shared/Infrastructure/Security/WebSecurityConfig.java
+++ src/main/java/com/webdev/tourapp/Shared/Infrastructure/Security/WebSecurityConfig.java
@@ -7,21 +7,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
-    //Todos los endpoints que esten permitidos como no usuario se debe de incluir acá.
+    //Todos los endpoints que esten permitidos como no usuario se debe de incluir acá.(GET)
     private static final String[] AUTH_WHITELIST = {
             "/v3/api-docs/**",
             "/v3/api-docs.yaml",
             "/swagger-ui/**"
+
     };
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception
+    {
         http.csrf().disable().
                 addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class).
                 authorizeRequests().
                 antMatchers(HttpMethod.POST, "/Users/Login").permitAll().
+                antMatchers(HttpMethod.POST, "/Guide/Login").permitAll().
+                antMatchers(HttpMethod.POST, "/Users/Create").permitAll().
+                antMatchers(HttpMethod.POST, "/Guide/Create").permitAll().
                 antMatchers(HttpMethod.GET, AUTH_WHITELIST).permitAll().
                 anyRequest().authenticated();
+
     }
 }
